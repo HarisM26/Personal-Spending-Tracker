03f26a1d86fccfc5f1e1a0bbc03805ceca0e3a2e
Richmond <richmond.bobie@outlook.com>
2023-01-27 00:36:57 +0000

    Add more tests for notification model

--- expenditure/models.py
+++ expenditure/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class Notification(models.Model):
   #user_receiver = models.ForeignKey(User,on_delete=models.CASCADE)
-  title = models.CharField(max_length=50, blank= False)
-  message = models.CharField(max_length = 500, blank = False)
+  title = models.CharField(max_length=300, blank= False)
+  message = models.CharField(max_length = 1200, blank = False)
   status = models.BooleanField(default=False)
-  created = models.DateTimeField(auto_now_add=True)
+  time_created = models.TimeField(auto_now_add=True, blank=False)
+  date_created = models.DateField(auto_now_add=True, blank=False)
 
