476b26d84bda5f23ad1357f061b5f2778641500b
MalikaBoss <malika.boss@kcl.ac.uk>
2023-01-29 11:52:49 +0000

    Create a transaction model -(without migrations)

--- expenditure/models.py
+++ expenditure/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class Transaction(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    notes = models.TextField(blank=True)
+    is_income = models.BooleanField(default=False)
+    reciept = models.ImageField(upload_to='', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
