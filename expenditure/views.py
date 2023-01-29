cd66399a19108f0c57b374a1b010e034c1b293de
Joel Jolly <joel.jolly@kcl.ac.uk>
2023-01-29 22:12:25 +0000

    working homepage with features

--- expenditure/views.py
+++ expenditure/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def features(request):
+    return render(request, 'features.html')
--- personal-spending-tracker/urls.py
+++ personal-spending-tracker/urls.py
@@ -20,4 +20,7 @@ from expenditure import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('features/', views.features, name='features'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
 ]
