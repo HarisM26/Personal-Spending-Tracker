02f66c3e3f0143f94b078c87d558e1efe99be228
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2023-01-30 11:08:04 +0000

    add log in and sign up form

--- expenditure/views.py
+++ expenditure/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import LogInForm, SignUpForm
+from .models import User
 
-# Create your views here.
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'sign_up.html' , {'form': form})
+
+
+def log_in(request):
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form})
--- personal-spending-tracker/urls.py
+++ personal-spending-tracker/urls.py
@@ -20,4 +20,6 @@ from expenditure import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('log_in/', views.log_in, name='log_in'),
 ]
