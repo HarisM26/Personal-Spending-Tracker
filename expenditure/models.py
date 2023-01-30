f6de2b406486d3ce12ef7bb6a7e9c6c1f7c6b3d8
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2023-01-30 02:04:13 +0000

    Add user model

--- expenditure/models.py
+++ expenditure/models.py
@@ -1,3 +1,11 @@
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django import forms
 
-# Create your models here.
+
+class User(AbstractUser):
+    password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
+   
+
+
