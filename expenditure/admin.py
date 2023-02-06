799e71485b19b25c3d9bedc14005a78ea3cab741
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2023-02-06 13:21:58 +0000

    users are now saved in the database, sign up and login works properly

--- expenditure/admin.py
+++ expenditure/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .forms import SignUpForm
+from .models import User
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
+    add_form = SignUpForm
+    model = User
     list_display = ("email", "is_staff", "is_active",)
     list_filter = ("email", "is_staff", "is_active",)
     fieldsets = (
@@ -28,5 +27,5 @@ class CustomUserAdmin(UserAdmin):
     ordering = ("email",)
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(User, CustomUserAdmin)
 # Register your models here.
--- expenditure/views.py
+++ expenditure/views.py
@@ -27,9 +27,9 @@ def log_in(request):
         form = LogInForm(request.POST)
 
         if form.is_valid():
-            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
             password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            user = authenticate(email=email, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('home')
