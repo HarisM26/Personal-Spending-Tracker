5aa16ccfe91c9544b829176f7f19b3308fa03f82
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2023-02-07 11:51:54 +0000

    edit asgi

--- personal-spending-tracker/asgi.py
+++ personal-spending-tracker/asgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'personal-spending-tracker.settings')
