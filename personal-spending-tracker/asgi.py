8a28ee1351e01a94db5646611da7ac42cec65b8a
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2023-02-07 12:07:04 +0000

    changes in requirements and asgi reverted

--- personal-spending-tracker/asgi.py
+++ personal-spending-tracker/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'personal-spending-tracker.settings')
@@ -19,4 +20,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 django_asgi_app = get_asgi_application()
 
 
-
+application = ProtocolTypeRouter({
+     "http": django_asgi_app,
+     # Just HTTP for now. (We can add other protocols later.)
+ })
