d5333f90d59d97bc7a887eccd0125ffd8198823d
Oreoluwa Fayemiwo <oreoluwa.fayemiwo@kcl.ac.uk>
2023-02-13 14:51:50 +0000

    Test user model, sign up view and sign up form

---
+++ expenditure/tests/forms/test_sign_up_form.py
@@ -0,0 +1,78 @@
+'''Unit test for the Sign Up Form'''
+from django import forms
+from django.test import TestCase
+from django.contrib.auth.hashers import check_password
+from expenditure.forms import SignUpForm
+from expenditure.models import User
+
+class SignUpFormTestCase(TestCase):
+    def setUp(self):
+    	self.form_input = {
+    	    'first_name': 'Will',
+            'last_name': 'Smith',
+            'email': 'willsmith@example.org',
+            'new_password': 'Password123',
+            'password_confirmation': 'Password123'
+    	}
+    
+    
+    #test that the form accepts input data
+    def test_valid_sign_up_form(self):
+    	form = SignUpForm(data=self.form_input)
+    	self.assertTrue(form.is_valid())
+    	
+    #test that the form has necessary fields
+    def test_form_has_necessary_fields(self):
+    	form = SignUpForm()
+    	self.assertIn('first_name',form.fields)
+    	self.assertIn('last_name',form.fields)
+    	
+    	self.assertIn('email',form.fields)
+    	email_field = form.fields['email']
+    	self.assertTrue(isinstance(email_field, forms.EmailField))
+    	
+    	self.assertIn('new_password',form.fields)
+    	new_password_widget = form.fields['new_password'].widget
+    	self.assertTrue(isinstance(new_password_widget, forms.PasswordInput))
+    	
+    	self.assertIn('password_confirmation',form.fields)
+    	password_confirmation_widget = form.fields['password_confirmation'].widget
+    	self.assertTrue(isinstance(password_confirmation_widget, forms.PasswordInput))
+    	
+    #test that the form uses model validation
+    def test_form_uses_model_validation(self):
+    	self.form_input['email'] = '@willsmith@gmail.com'
+    	form = SignUpForm(data=self.form_input)
+    	self.assertFalse(form.is_valid())
+    	
+    def test_password_must_contain_uppercase_character(self):
+    	self.form_input['new_password'] = 'hello123'
+    	self.form_input['password_confirmation'] = 'hello123'
+    	form = SignUpForm(data=self.form_input)
+    	self.assertFalse(form.is_valid())
+    	
+    def test_password_must_contain_lowercase_character(self):
+    	self.form_input['new_password'] = 'HELLO123'
+    	self.form_input['password_confirmation'] = 'HELLO123'
+    	form = SignUpForm(data=self.form_input)
+    	self.assertFalse(form.is_valid())
+    
+    def test_password_must_contain_number(self):
+    	self.form_input['new_password'] = 'helloHELLO'
+    	self.form_input['password_confirmation'] = 'helloHELLO'
+    	form = SignUpForm(data=self.form_input)
+    	self.assertFalse(form.is_valid())
+    	
+    def test_form_must_save_correctly(self):
+    	form = SignUpForm(data=self.form_input)
+    	before_count = User.objects.count()
+    	form.save()
+    	after_count = User.objects.count()
+    	self.assertEqual(after_count, before_count + 1)
+    	user = User.objects.get(email = 'willsmith@example.org')
+    	self.assertEqual(user.first_name, 'will')
+    	self.assertEqual(user.last_name, 'smith')
+    	is_password_correct = check_password('Password123', user.password)
+    	self.assertTrue(is_password_correct)
+    
+   
---
+++ expenditure/tests/views/test_sign_up_view.py
@@ -0,0 +1,54 @@
+'''Unit test for the Sign Up View'''
+from django.test import TestCase
+from django.urls import reverse 
+from django.contrib.auth.hashers import check_password
+from expenditure.forms import SignUpForm
+from expenditure.models import User
+
+class SignUpFormTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('sign_up')
+        self.form_input = {
+    	    'first_name': 'Will',
+            'last_name': 'Smith',
+            'email': 'willsmith@example.org',
+            'new_password': 'Password123',
+            'password_confirmation': 'Password123'
+    	}
+    	
+    def test_sign_up_url(self):
+    	self.assertEqual(self.url,'/sign_up/')
+    
+    def test_get_sign_up(self):
+    	response = self.client.get(self.url)
+    	self.assertEqual(response.status_code, 200)
+    	self.assertTemplateUsed(response, 'sign_up.html')
+    	form = response.context['form']
+    	self.assertTrue(isinstance(form, SignUpForm))
+    	self.assertFalse(form.is_bound)
+    	
+    def test_unsuccessful_sign_up(self):
+    	self.form_input['email'] = '@willsmith@example.org'
+    	before_count = User.objects.count()
+    	response = self.client.post(self.url, self.form_input)
+    	after_count = User.objects.count()
+    	self.assertEqual(after_count, before_count)
+    	self.assertEqual(response.status_code, 200)
+    	self.assertTemplateUsed(response, 'sign_up.html')
+    	form = response.context['form']
+    	self.assertTrue(isinstance(form, SignUpForm))
+    	self.assertTrue(form.is_bound)
+    	
+    def successful_sign_up(self):
+    	before_count = User.objects.count()
+    	response = self.client.post(self.url, self.form_input, follow=True)
+    	after_count = User.objects.count()
+    	self.assertEqual(after_count, before_count + 1)
+    	response_url = reverse('about')
+    	self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+    	self.assertTemplateUsed(response, 'about.html')
+    	user = User.objects.get(email = 'willsmith@example.org')
+    	self.assertEqual(user.first_name, 'will')
+    	self.assertEqual(user.last_name, 'smith')
+    	is_password_correct = check_password('Password123', user.password)
+    	self.assertTrue(is_password_correct)
