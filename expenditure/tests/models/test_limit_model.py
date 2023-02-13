f3db2aab6d144254033fa7ecff65cc3897d9b354
Konrad Bylina <konrad.bylina@kcl.ac.uk>
2023-02-13 14:45:12 +0000

    Add tests to test_limit_model

--- expenditure/tests/models/test_limit_model.py
+++ expenditure/tests/models/test_limit_model.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from datetime import datetime
+from datetime import timedelta,datetime
 from django.core.exceptions import ValidationError
 from expenditure.models import Category,Limit,User
 from decimal import *
@@ -21,24 +21,80 @@ class LimitModelTest(TestCase):
     def assert_limit_is_valid(self):
         try:
             self.limit.full_clean()
-        except:
-            self.fail('Limit should be valid')
+        except (ValidationError):
+            self.fail('Limit is not valid')
     
     def assert_limit_is_invalid(self):
         with self.assertRaises(ValidationError):
             self.limit.full_clean()
     
-    def test_limit(self):
+    def test_limit_is_valid(self):
         self.assert_limit_is_valid()
-
 
     def test_spent_amount_cannot_be_blank(self):
         self.limit.spent_amount = ''
         self.assert_limit_is_invalid()
 
-    def test_spent_amount_cannot_be_negative(self):
-        self.limit.spent_amount = -1
+    def test_limit_amount_cannot_be_negative(self):
+        self.limit.limit_amount = -1000.00
         self.assert_limit_is_invalid()
+
+    def test_limit_amount_cannot_be_zero(self):
+        self.limit.limit_amount = 0.00
+        self.assert_limit_is_invalid()
+    
+    def test_limit_amount_cannot_have_more_than_2_decimal_places(self):
+        self.limit.limit_amount = 1000.001
+        self.assert_limit_is_invalid()
+    
+    def test_limit_amount_can_have_10_digits(self):
+        self.limit.limit_amount = 12345678.91
+        self.assert_limit_is_valid()
+
+    def test_limit_amount_cannot_have_more_than_10_digits(self):
+        self.limit.limit_amount = 123456789.00
+        self.assert_limit_is_invalid()
+    
+    def test_limit_amount_cannot_be_null(self):
+        self.limit.limit_amount = None
+        self.assert_limit_is_invalid()
+
+    def test_remaining_amount_cannot_have_more_than_2_decimal_places(self):
+        self.limit.remaining_amount = 1000.001
+        self.assert_limit_is_invalid()
+    
+    def test_remaining_amount_can_have_10_digits(self):
+        self.limit.remaining_amount = 12345678.91
+        self.assert_limit_is_valid()
+    
+    def test_remaining_amount_cannot_have_more_than_10_digits(self):
+        self.limit.remaining_amount = 123456789.00
+        self.assert_limit_is_invalid()
+    
+    def test_remaining_amount_is_zero_by_default(self):
+        self.assertEqual(self.limit.remaining_amount, 0.00)
+    
+    def test_status_is_not_reached_by_default(self):
+        self.assertEqual(self.limit.status, 'not reached')
+
+    # def test_correct_default_values(self):
+    #    self.assertEqual(self.limit.status, 'not reached')
+    #    self.assertEqual(self.limit.time_limit_type, 'weekly')
+
+    # def test_calculate_limit_approaching(self):
+    #    self.limit.spent_amount = 900
+    #    self.limit.save()
+    #    self.assertEqual(self.limit.status, 'approaching')
+    
+    # def test_calculate_limit_reached(self):
+    #    self.limit.spent_amount = 1000
+    #    self.limit.save()
+    #    self.assertEqual(self.limit.status,'reached')
+    
+    # def test_calculate_limit_not_reached(self):
+    #    self.limit.spent_amount = 899
+    #    self.limit.save()
+    #    self.assertEqual(self.limit.status, 'not reached')
 
     def test_setLimitAmount_assigns_valid_limit(self):
         self.limit.setLimitAmount(-1)
@@ -73,4 +129,21 @@ class LimitModelTest(TestCase):
             self.assert_limit_is_valid()
         else:
             self.assert_limit_is_invalid()
+
+    """k"""
+    
+    
+
+    
+    
+    
+    
+    
+
+    #TODO: add tests for start_date and end_date (end_date cannot be before start_date)
+    
+    
+
+
+
     
