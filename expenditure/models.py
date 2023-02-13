f0fca7880bd26ffb389aedc956c5d812a140ad4e
Konrad Bylina <konrad.bylina@kcl.ac.uk>
2023-02-13 14:46:32 +0000

    Add import for MinValueValidator

--- expenditure/models.py
+++ expenditure/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from datetime import datetime, date, timedelta
 from decimal import Decimal
+from django.core.validators import MinValueValidator
 
 
 class UserManager(BaseUserManager):
