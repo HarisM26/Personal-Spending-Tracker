392e86e98fd72a95aa676ad156f3977088d80b3d
Joel Jolly <joel.jolly@kcl.ac.uk>
2023-02-25 14:21:08 +0000

    homepage clean-up

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -125,55 +125,36 @@ function getFileName(fullPath) {
 }
 
 
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 4,
-//     spaceBetween: 20,
-//     sliderPerGroup: 4,
-//     loop: true,
-//     centerSlide: "true",
-//     fade: "true",
-//     grabCursor: "true",
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//       dynamicBullets: true,
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-  
-//     breakpoints: {
-//       0: {
-//         slidesPerView: 1,
-//       },
-//       520: {
-//         slidesPerView: 2,
-//       },
-//       768: {
-//         slidesPerView: 3,
-//       },
-//       1000: {
-//         slidesPerView: 4,
-//       },
-//     },
-//   });
-  
-
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    sliderPerGroup: 4,
-    loop: true,
-    centerSlide: "true",
-    fade: "true",
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    520: {
+      slidesPerView: 2,
     },
-  });
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+});
