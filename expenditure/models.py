f530d9103f0e31b3611afe3272b2273d4464ae32
Konrad Bylina <konrad.bylina@kcl.ac.uk>
2023-03-12 15:28:27 +0000

    Add PasswordResetToken model

--- expenditure/models.py
+++ expenditure/models.py
@@ -103,6 +103,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
 
+class PasswordResetToken(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
