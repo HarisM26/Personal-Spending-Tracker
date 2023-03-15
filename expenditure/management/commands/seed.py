5ec8c9e30954b08e9bd1c2126a508909035c80d6
Konrad Bylina <konrad.bylina@kcl.ac.uk>
2023-03-15 00:12:18 +0000

    seeded users follow random number of other users

--- expenditure/management/commands/seed.py
+++ expenditure/management/commands/seed.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError
 
 from expenditure.models import User
+from random import randint, choice, sample
 
 
 from faker import Faker
@@ -28,14 +29,14 @@ class Command(BaseCommand):
         new_seeded_users = 0
         while new_seeded_users < self.USER_COUNT:
             try:
-                self.create_user()
+                self.create_user(new_seeded_users)
                 new_seeded_users += 1
                 self.stdout.write(self.style.SUCCESS(
                     f'User {new_seeded_users} seeded'))
             except IntegrityError:
                 continue
 
-    def create_user(self):
+    def create_user(self, new_seeded_users):
         first_name = self.faker.first_name()
         last_name = self.faker.last_name()
         email = generate_email(first_name, last_name)
@@ -48,6 +49,15 @@ class Command(BaseCommand):
             is_staff=False,
         )
         user.set_password(self.DEFAULT_PASSWORD)
+        self.add_followings(user, new_seeded_users)
+
+    def add_followings(self, user, new_seeded_users):
+        self.stdout.write(self.style.SUCCESS('Adding followings...'))
+        num_following = randint(0, new_seeded_users)
+        all_users = list(User.objects.all())
+        random_users = sample(all_users, num_following)
+        for random_user in random_users:
+            user.toggle_follow(random_user)
 
 
 def generate_email(first_name, last_name):
