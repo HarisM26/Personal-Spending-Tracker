842c99cbd302c67fe62046d6cfe2dea70c7fda9c
Konrad Bylina <konrad.bylina@kcl.ac.uk>
2023-03-21 15:31:47 +0000

    Send emails when limits reached or approaching

--- expenditure/signals.py
+++ expenditure/signals.py
@@ -7,6 +7,8 @@ from django.contrib.auth import get_user_model
 from expenditure.models.categories import *
 from expenditure.models.transactions import *
 from expenditure.models.user import *
+from .email_manager import EmailSender
+from .helpers import get_percentage_of_limit_used
 
 User = get_user_model()
 
@@ -31,6 +33,7 @@ def update_remaining_amount(instance, created, *args, **kwargs):
         limit = instance.spending_category.limit
         limit.remaining_amount = limit.limit_amount-total
         limit.save()
+
 
 @receiver(post_save, sender=SpendingCategory)
 def update_remaining_when_category_edited(instance, created, *args, **kwargs):
@@ -62,6 +65,8 @@ def transaction_post_save_handler(instance, created, *args, **kwargs):
             notification = create_limit_notification(
                 current_user, instance.spending_category.name, instance.spending_category.limit, total)
             notification.save()
+            EmailSender().send_approaching_limit_email(current_user, instance.spending_category.name,
+                                                       get_percentage_of_limit_used(instance.spending_category.limit))
             limit = instance.spending_category.limit
             limit.status = 'approaching'
             limit.save()
@@ -69,9 +74,12 @@ def transaction_post_save_handler(instance, created, *args, **kwargs):
             notification = create_limit_notification(
                 current_user, instance.spending_category.name, instance.spending_category.limit, total)
             notification.save()
+            EmailSender().send_reached_limit_email(
+                current_user, instance.spending_category.name)
             limit = instance.spending_category.limit
             limit.status = 'reached'
             limit.save()
+
 
 @receiver(post_save, sender=SpendingCategory)
 def category_post_save_handler(instance, created, *args, **kwargs):
@@ -79,27 +87,31 @@ def category_post_save_handler(instance, created, *args, **kwargs):
     if not created and current_user.toggle_notification == 'ON':
         # filter current transactions in particular category
         all_transactions = SpendingTransaction.objects.filter(
-                spending_category=instance, is_current=True)
+            spending_category=instance, is_current=True)
         total = Decimal('0.00')
         for transaction in all_transactions:
             total += transaction.amount
 
         if total >= (instance.limit.calc_90_percent_of_limit) and \
-                    total < Decimal(instance.limit.limit_amount):
-                notification = create_limit_notification(
-                    current_user, instance.name, instance.limit, total)
-                notification.save()
-                limit = instance.limit
-                limit.status = 'approaching'
-                limit.save()
+                total < Decimal(instance.limit.limit_amount):
+            notification = create_limit_notification(
+                current_user, instance.name, instance.limit, total)
+            notification.save()
+            EmailSender().send_approaching_limit_email(current_user, instance.spending_category.name,
+                                                       get_percentage_of_limit_used(instance.spending_category.limit))
+            limit = instance.limit
+            limit.status = 'approaching'
+            limit.save()
         elif total >= (instance.limit.calc_90_percent_of_limit):
             notification = create_limit_notification(
                 current_user, instance.name, instance.limit, total)
             notification.save()
+            EmailSender().send_reached_limit_email(
+                current_user, instance.spending_category.name)
             limit = instance.limit
             limit.status = 'reached'
             limit.save()
-    
+
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
