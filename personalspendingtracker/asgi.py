263e10ace90081971b500d4023dfc0ea273e8c79
Richmond <richmond.bobie@outlook.com>
2023-03-23 17:39:06 +0000

    change icons

--- personalspendingtracker/asgi.py
+++ personalspendingtracker/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for personal-spending-tracker project.
+ASGI config for personalspendingtracker project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -12,9 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'personal-spending-tracker.settings')
+                      'personalspendingtracker.settings')
 
 application = get_asgi_application()
-
-
-
