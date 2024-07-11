522784fffd107db681f6bdd34d05a84621507343
Konrad Bylina <54275427+kondzio13@users.noreply.github.com>
2024-07-11 14:52:08 +0100

    Update settings.py

--- personal-spending-tracker/settings.py
+++ personal-spending-tracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tpfuxvw7-9jg#o@7&57&@^9ao$=5i^krv%@g)9&!6g_ueb5u$l'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
